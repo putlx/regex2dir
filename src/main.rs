f27a750425bef563c29fda5cd217ef6d21203da6
putlx <putlx@outlook.com>
2022-01-18 15:40:56 +0800

    Upgrade to clap 3.0

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use clap::{App, AppSettings, Arg};
+use clap::Parser;
 use regex_automata::{dense, DFA};
 use std::collections::{hash_map, HashMap};
 use std::{error, fs, io, path};
@@ -35,22 +35,22 @@ fn walk<T: DFA>(
     Ok(())
 }
 
+#[derive(Parser)]
+#[clap(version)]
+struct Args {
+    /// Anchor regex at beginning
+    #[clap(short, long)]
+    anchor: bool,
+    pattern: String,
+    directory: String,
+}
+
 fn main() -> Result<(), Box<dyn error::Error>> {
-    let matches = App::new("regex2dir")
-        .setting(AppSettings::DisableVersion)
-        .arg(
-            Arg::with_name("anchor")
-                .short("a")
-                .long("anchor")
-                .help("Anchor regex at beginning"),
-        )
-        .arg(Arg::with_name("PATTERN").required(true).index(1))
-        .arg(Arg::with_name("DIRECTORY").required(true).index(2))
-        .get_matches();
+    let args = Args::parse();
     let dfa = dense::Builder::new()
-        .anchored(matches.is_present("anchor"))
-        .build(matches.value_of("PATTERN").unwrap())?;
-    let root = matches.value_of("DIRECTORY").unwrap();
+        .anchored(args.anchor)
+        .build(&args.pattern)?;
+    let root = &args.directory;
     fs::create_dir(root)?;
     let mut state_to_dir = HashMap::new();
     state_to_dir.insert(
